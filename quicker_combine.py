e6bba03413f9dd6fbca074db32a866a867e15f24
Elvira <40444030+Ellie1994@users.noreply.github.com>
2018-06-23 00:53:33 +0200

    Add files via upload

---
+++ quicker_combine.py
@@ -0,0 +1,5 @@
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+my_list = [(letter, num) for letter in "abcd" for num in range(5)]
+
+print(my_list)
---
+++ stand_combine.py
@@ -0,0 +1,8 @@
+nums = [1,2,3,4,5,6,7,8,9,10]
+my_list = []
+
+for letter in "abcd":
+    for num in range(5): 
+        my_list.append((letter,num))
+
+print(my_list)
